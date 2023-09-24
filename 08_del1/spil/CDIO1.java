b4e0c139587268a7b8b382570824ddbdeb329a3e
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2023-09-24 12:37:56 +0200

    Explainations added

--- 08_del1/spil/CDIO1.java
+++ 08_del1/spil/CDIO1.java
@@ -2,11 +2,15 @@ import java.lang.Math;
 
 class Diceroll{
     public static void main(String args[]){
+
+        //We get a random double value (0 to 1) from the Math.random
         var randomValue = Math.random();
 
         //Here we convert the double value from the Math.random to an integer
         //and we also have to use Math.floor to round down
         int dice = (int) Math.floor(randomValue*6)+1; 
+
+        //To check if the dice work
         System.out.println(dice);
     } 
 }
