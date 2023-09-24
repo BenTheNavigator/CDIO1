ee99c9f63786ddbfa29072b03f644e8643d75c37
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2023-09-24 12:46:55 +0200

    Second die + Sum

--- 08_del1/spil/CDIO1.java
+++ 08_del1/spil/CDIO1.java
@@ -3,12 +3,17 @@ class Diceroll{
 
         //We get a random double value (0 to 1) from the Math.random
         var randomValue = Math.random();
+        var randomValue2 = Math.random();
 
         //Here we convert the double value from the Math.random to an integer
         //and we also have to use Math.floor to round down
         int dice = (int) Math.floor(randomValue*6)+1; 
-
+        int dice2 = (int) Math.floor(randomValue2*6)+1; 
+        //We find the sum, which is the important part
+        var diceSum = dice+dice2;
         //To check if the dice work
-        System.out.println(dice);
+        System.out.println("First dice: " + dice);
+        System.out.println("Second dice: " + dice2);
+        System.out.println("Sum: " + diceSum);
     } 
 }
