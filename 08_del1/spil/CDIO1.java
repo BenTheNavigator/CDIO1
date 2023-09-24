c1848c68b85889f693b655d3d89f92682d7f180c
BenTheNavigator <benjaminlgad@gmail.com>
2023-09-24 12:04:11 +0200

    scanner and player name

--- 08_del1/spil/CDIO1.java
+++ 08_del1/spil/CDIO1.java
@@ -1,8 +1,26 @@
 class RAFFLEMANIA {
     public static void main (String args[]){
+        System.out.print("spilbeskrivelse");
+
+        var scanner = new java.util.Scanner(System.in);
+        var point1 = 0;
+        var point2 = 0;
+
+        System.out.println("What is player 1's name?");
+        var player1name = scanner.nextLine();
+        System.out.println("May the RAFFLEMANIA be with you " + player1name + "!");
 
 
 
+
+
+
+
+
+
+
+
+        scanner.close();
     }
 
 
